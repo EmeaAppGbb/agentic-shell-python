5e2474aa09bb154c9118a3da2fab7895fd7a6eb4
Kosta Petan <kostapetan@gmail.com>
2025-11-27 15:49:39 +0100

    initial

---
+++ apphost.cs
@@ -0,0 +1,31 @@
+﻿
+
+#:sdk Aspire.AppHost.Sdk@13.0.0
+#:package Aspire.Hosting.Python@13.0.0
+#:package Aspire.Hosting.JavaScript@13.0.0
+#:package Aspire.Hosting.Azure.CognitiveServices@13.0.0
+#:package Aspire.Hosting.Azure.AIFoundry@13.0.0-preview.1.25560.3
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+var openAiEndpoint = builder.AddParameter("openAiEndpoint");
+var openAiDeployment = builder.AddParameter("openAiDeployment");
+
+var api = builder.AddUvicornApp("agentic-api", "./src/agentic-api", "main:app")
+    .WithUv()
+    .WithEnvironment("AZURE_OPENAI_ENDPOINT", openAiEndpoint)
+    .WithEnvironment("AZURE_OPENAI_DEPLOYMENT_NAME", openAiDeployment)
+    .WithExternalHttpEndpoints()
+    .WithHttpHealthCheck("/health");
+
+builder.AddJavaScriptApp("agentic-ui", "./src/agentic-ui")
+    .WithRunScript("dev")
+    .WithNpm(installCommand: "ci")
+    .WithEnvironment("AGENT_API_URL", api.GetEndpoint("http"))
+    .WithReference(api)
+    .WaitFor(api)
+    .WithHttpEndpoint(env: "PORT")
+    .WithExternalHttpEndpoints()
+    .PublishAsDockerFile();
+
+builder.Build().Run();
